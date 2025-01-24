42b82a5c2d95277646ef5dea94bc36bd1e08c0b5
Auscan7 <auscan7@gmail.com>
2025-01-24 21:25:50 +0300

    Folder organization, New art

--- Assets/Scripts/Item/Pellet.cs
+++ Assets/Scripts/Item/Pellet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pellet : MonoBehaviour
@@ -11,7 +12,7 @@ public class Pellet : MonoBehaviour
         pelletDamage = WeaponManager.Instance.shotgunDamage;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
